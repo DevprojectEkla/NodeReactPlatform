0e75b082738bff110a99ac2979a876f1bc57000e
DevEkla <barlavigabriel@gmail.com>
2024-05-27 14:12:09 +0000

    deploy script .scripts/deploy_NodePlatform.sh updated

--- .scripts/deploy_NodePlatform.sh
+++ .scripts/deploy_NodePlatform.sh
@@ -9,6 +9,8 @@
 ######################################################################
 
 cd /home/ekla/NodeReactPlatform/
+eval $(ssh-agent)
+ssh-add /home/ekla/.ssh/id_rsa_github
 git pull origin main
 pm2 restart all
 
