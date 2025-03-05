31aa4e3818b719f79566396fcaf35f0538d5c435
Ekla <108277748+DevprojectEkla@users.noreply.github.com>
2025-03-05 23:29:33 +0100

    Revert "fix(debug): debug variable fix"

--- server/wasm/utils_wasm_lib.d.ts
+++ server/wasm/utils_wasm_lib.d.ts
@@ -1,6 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
-export function start(): void;
 export function add(a: number, b: number): string;
 export function hello(message: string): void;
 export function encode_base64(input: Uint8Array): string;
